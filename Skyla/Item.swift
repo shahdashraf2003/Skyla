9a920d2cee856abb4d9990f4f198c2fafe81626a
shahd <shahdashraf396@gmail.com>
2026-05-26 13:15:53 -0700

    Initial Commit

---
+++ Skyla/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  Skyla
+//
+//  Created by TaqieAllah on 26/05/2026.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
